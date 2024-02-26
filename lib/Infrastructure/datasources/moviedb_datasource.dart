181087500d3b548568b0ddf36f113383cbd95b56
Yago Otero <Yagootero2003@gmail.com>
2024-02-26 23:22:31 +0100

    Implementacion de mappers anadido

--- lib/Infrastructure/datasources/moviedb_datasource.dart
+++ lib/Infrastructure/datasources/moviedb_datasource.dart
@@ -1,3 +1,5 @@
+import 'package:cinemapedia/Infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/Infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:cinemapedia/config/constants/enviromenment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
@@ -14,7 +16,13 @@ class MoviedbDatasource extends MoviesDataSource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
+
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieDBResponse.results
+    .where((moviedb) => moviedb.posterPath != 'no-poster')
+    .map(
+      (moviedb) => MovieMapper.movieDBToEntity(moviedb)).toList();
 
     return movies;
   }
